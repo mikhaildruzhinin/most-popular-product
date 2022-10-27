85315163b414f98747b389674b33d410e49a34dd
Mikhail Druzhinin <druzhininmy@gmail.com>
2022-10-27 14:59:18 +0300

    add schemas

---
+++ src/main/scala/application/Schemas.scala
@@ -0,0 +1,35 @@
+package application
+
+import org.apache.spark.sql.types._
+
+object Schemas {
+  val customerSchema: StructType = StructType(
+    Array(
+      StructField("id", IntegerType),
+      StructField("name", StringType),
+      StructField("email", StringType),
+      StructField("joinDate", DateType),
+      StructField("status", StringType)
+    )
+  )
+
+  val orderSchema: StructType = StructType(
+    Array(
+      StructField("customerID", IntegerType),
+      StructField("orderID", IntegerType),
+      StructField("productID", IntegerType),
+      StructField("numberOfProduct", IntegerType), // кол-во товара в заказе
+      StructField("orderDate", DateType),
+      StructField("status", StringType)
+    )
+  )
+
+  val productSchema: StructType = StructType(
+    Array(
+      StructField("id", IntegerType),
+      StructField("name", StringType),
+      StructField("price", DoubleType),
+      StructField("numberOfProducts", IntegerType)
+    )
+  )
+}
